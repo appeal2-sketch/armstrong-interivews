2c7d3dd452ad5d39ed749cb2a231f921f15d8c8c
appeal2-sketch <appeal2@me.com>
2026-09-11 11:24:24 -0400

    Create fetch-videos.js

---
+++ netlify/scripts/fetch-videos.js
@@ -0,0 +1,9 @@
+[
+  {
+    "id": "mwYNY7kwQIw",
+    "title": "Martin Armstrong Economic Confidence Model & Cycle Forecasts",
+    "host": "Armstrong Economics Archive",
+    "date": "2026-09-01",
+    "url": "https://www.youtube.com/watch?v=mwYNY7kwQIw"
+  }
+]
