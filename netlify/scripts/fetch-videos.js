8991ef83812c19e75c1754e455b77df2a2fd8403
appeal2-sketch <appeal2@me.com>
2026-09-11 11:32:58 -0400

    Delete netlify/scripts directory

--- netlify/scripts/fetch-videos.js
+++
@@ -1,9 +0,0 @@
-[
-  {
-    "id": "mwYNY7kwQIw",
-    "title": "Martin Armstrong Economic Confidence Model & Cycle Forecasts",
-    "host": "Armstrong Economics Archive",
-    "date": "2026-09-01",
-    "url": "https://www.youtube.com/watch?v=mwYNY7kwQIw"
-  }
-]
